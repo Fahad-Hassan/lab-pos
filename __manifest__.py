153e06d042bd81d8f2153b5800d1d65437c436cb
Fahad Hassan <fahadhassan668@gmail.com>
2022-10-10 18:51:48 +0400

    deploy finalyy

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
             From the registration of patients and their samples to 
             the sending of the analysis report.
                 """,
-    'description': """Manarerergement bkbkhossssfjbjb adfdfdf medical laboratory """,
+    'description': """Manarerergement bretretretrekbkhossssfjbjb adfdfdf medical laboratory """,
     'version': '14.0.1.0',
     'sequence':15,
     'website': "",
